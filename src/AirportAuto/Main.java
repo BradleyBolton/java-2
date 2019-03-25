c68ad37a10c3c56554a7cc0a366addf4178ccf53
BradleyBolton <boltonb19@greenhill.org>
2019-03-25 14:18:45 -0500

    blank

--- src/AirportAuto/Main.java
+++ src/AirportAuto/Main.java
@@ -3,15 +3,16 @@ package AirportAuto;
 public class Main
 {
     //Main for Auto
-    /*public static void main(String args[])
+    public static void main(String args[])
     {
         auto car1 = new auto("Ford", 9, 7.8);
-        auto.car2 = new auto ("Chevy", this.miles, this.gallons);
+        auto car2 = new auto ("Chevy", this.miles, this.gallons);
         System.out.println(car1.toString());
-    }*/
+        System.out.println(car2.toString());
+    }
 
     //Main for Airport
-    public static void main(String args[])
+    /*public static void main(String args[])
     {
         Airport airport1 = new Airport("DFW", 8);
         auto car1 = new auto("Ford", 11, 6);
@@ -26,5 +27,5 @@ public class Main
         {
             System.out.println ("Airports and cars are different.");
         }
-    }
+    }*/
 }
