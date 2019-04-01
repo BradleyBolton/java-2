47febc707b99bb068dde59ac89669b1fe2a5c8bf
BradleyBolton <boltonb19@greenhill.org>
2019-04-01 14:56:18 -0500

    Yearly weather report graphic

---
+++ ClassPrograms/javaFX/temperatureGraphClientWithArrays.java
@@ -0,0 +1,53 @@
+package javaFX;
+
+import javafx.application.Application;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.stage.Stage;
+import java.util.*;
+
+public class temperatureGraphClientWithArrays extends Application
+{
+    @Override
+    public void start(Stage stage)
+    {
+        Scanner scan = new Scanner(System.in);
+        int temperature;
+        temperatureGraphObject [] month = new temperatureGraphObject[12];
+        String [] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        int lowTemp = 1000;
+        int highTemp = 0;
+        int lowLimit = 0;
+        int highLimit = 40;
+        int flip = 1;
+        for (int i = 0; i < 12; i++) {
+            System.out.println("temperature in "+months[i]+"- ");
+            temperature = scan.nextInt();
+            if (temperature > highLimit || temperature < lowLimit)
+            {
+                System.out.println("The input temperature seems odd and may have been an input error. Input intended temperature for "+months[i]+" to confirm.");
+                temperature = scan.nextInt();
+            }
+            month[i] = new temperatureGraphObject(0, 0, 1, temperature, months[i]);
+            if (lowTemp > temperature) { lowTemp = temperature; }
+            if (highTemp < temperature) {highTemp = temperature; }
+            if (months[i].equals("Jul")) { flip = flip * -1; }
+            lowLimit += 10*flip;
+            highLimit += 10*flip;
+        }
+        GraphicsContext gc = JIGRaphicsUtility.setUpGraphics(stage, "One year of monthly temperatures", 490, highTemp * 8 + 100); //height and width are backwards
+        int x = 10;
+        for (int i = 0; i < 12; i++)
+        {
+            month[i].setCoordinates( x, (highTemp * 8 + 50) - (month[i].getTemperature() * 8));
+            month[i].draw(gc);
+            x += 40;
+        }
+        gc.fillText("The max temperature was "+highTemp+".", 10, highTemp * 8 + 65);
+        gc.fillText("The min temperature was "+lowTemp+".", 10, highTemp * 8 + 75);
+    }
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
+}
+
--- ClassPrograms/javaFX/temperatureGraphObject.java
+++ ClassPrograms/javaFX/temperatureGraphObject.java
@@ -32,23 +32,12 @@ public class temperatureGraphObject
         this.scale = (scale > 0 ? scale:this.scale);
         return this;
     }
+    public int getTemperature()
+    {
+        return temperature;
+    }
     public void draw(GraphicsContext gc)
     {
-        /*gc.setFill(Color.CORAL);
-        gc.fillOval(sX, sY + 15 * scale, 90*scale, 120*scale);
-        gc.setFill(Color.DARKGOLDENROD);
-        gc.fillRect(sX + 23 * scale, sY, 45 * scale, 22 * scale);
-        gc.setStroke(Color.DARKGOLDENROD);
-        gc.setLineWidth(3);
-        gc.strokeLine(sX, sY + 23 * scale, sX + 90 * scale, sY + 23 * scale);
-        gc.setFill(Color.CHOCOLATE);
-        gc.fillOval(sX + 60 * scale, sY + 45 * scale, 18 * scale, 12 * scale);
-        gc.setFill(Color.DARKSALMON);
-        gc.setLineWidth(1);
-        gc.fillOval(sX+45*scale, sY + 125 * scale, 45 * scale, 12 * scale);
-        gc.strokeOval(sX + 45 * scale, sY + 125 * scale, 45 * scale, 12 * scale);
-        gc.fillOval(sX + 27 * scale, sY + 127*scale, 45 * scale, 12 * scale);
-        gc.strokeOval(sX + 27 * scale, sY + 127*scale, 45 * scale, 12 * scale);*/
         if (temperature < 50) { gc.setFill(Color.BLUE); }
         else if (temperature > 77) { gc.setFill(Color.RED); }
         else { gc.setFill(Color.YELLOW); }
@@ -57,6 +46,6 @@ public class temperatureGraphObject
         gc.fillText(month, sX, sY - 24);
         String temp = ""+temperature;
         gc.fillText(temp, sX + 5, sY - 8);
-        System.out.println("Printing a rectangle. sX = "+sX+". sY = "+sY+". Height is "+temperature * 8+"."); //for debug
+        //System.out.println("Printing a rectangle. sX = "+sX+". sY = "+sY+". Height is "+temperature * 8+"."); //for debug
     }
 }
