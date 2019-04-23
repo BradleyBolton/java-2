75a2daa112a48ec18c04e71ec8082f4c894ba2b5
BradleyBolton <boltonb19@greenhill.org>
2019-04-23 15:46:09 -0500

    Inheritance Practice Programs

--- ClassPrograms/inheritancePractice/employeeClient.java
+++ ClassPrograms/inheritancePractice/employeeClient.java
@@ -4,9 +4,17 @@ public class employeeClient
 {
     public static void main(String [] args)
     {
-        Employee ed = new Lawyer(5);
-        System.out.println(ed.getSalary());
-        System.out.println(ed.getVacationForm());
-        System.out.println(ed.getVacationDays());
+        Employee [] employees = {new Lawyer(10), new Secretary(3), new legalSecretary(5)};
+        for (int i = 0; i < employees.length; i++)
+        {
+            printInfo(employees[i]);
+        }
+    }
+    public static void printInfo(Employee employee)
+    {
+        System.out.println("Salary- "+employee.getSalary());
+        System.out.println("v.days- "+employee.getVacationDays());
+        System.out.println("v.form- "+employee.getVacationForm());
+        System.out.println();
     }
 }
